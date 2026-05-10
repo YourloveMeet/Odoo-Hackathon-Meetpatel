157035ad1fd117f768a672dbb3381545fce7f3ee
Meet Patel <95916676+YourloveMeet@users.noreply.github.com>
2026-05-10 11:57:25 +0530

    Sorting changes

--- Backend/models/City.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const citySchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    country: { type: String, required: true },
-    imageUrl: { type: String }
-}, { timestamps: true });
-
-module.exports = mongoose.model("City", citySchema);
